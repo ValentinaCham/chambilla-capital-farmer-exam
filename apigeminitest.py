e10737b3e2f166b0089b585288ffd9b5bba53363
Valentina Chambilla <vchambillap@unsa.edu.oe>
2025-05-26 14:22:12 -0500

    fix: model gemini API

--- apigeminitest.py
+++ apigeminitest.py
@@ -37,7 +37,7 @@ safety_settings = [
     },
 ]
 
-model = genai.GenerativeModel(model_name="gemini-pro",
+model = genai.GenerativeModel(model_name="gemini-1.5-flash",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
